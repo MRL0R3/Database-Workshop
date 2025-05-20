67f3517f7caa1102776688fee9f08a1e4433822e
MRL0R3 <ahmadrezajafary1384@gmail.com>
2025-05-20 19:27:06 +0330

    creating new player

--- Database-Workshop/src/main/java/org/project/DatabaseManager.java
+++ Database-Workshop/src/main/java/org/project/DatabaseManager.java
@@ -11,7 +11,7 @@ public class DatabaseManager {
     // todo: modify these constants to connect to database
     private static final String JDBC_URL = "jdbc:postgresql://localhost:5432/workshop";
     private static final String USERNAME = "postgres";
-    private static final String PASSWORD = "password";
+    private static final String PASSWORD = "35546";
 
     // a method to connect to database with credentials given
     private static Connection connect() throws SQLException {
@@ -21,8 +21,11 @@ public class DatabaseManager {
     public static void createPlayer(Player player) throws SQLException {
         Connection conn = connect();
         // todo: fill the query to create a player
-        String query = "";
+        String query = "INSERT INTO player(id , full_name , club_id) VALUES (? , ? , ?)";
         PreparedStatement ps = conn.prepareStatement(query);
+        ps.setObject(1 , player.getId());
+        ps.setString(2 , player.getFullName());
+        ps.setObject(3 , player.getClubId());
         // todo: set values of the prepared statement
         ps.executeUpdate();
         ps.close();
