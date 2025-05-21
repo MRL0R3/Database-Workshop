15f0891aa4d2975fd65b2018399adb0a89711c1d
MRL0R3 <ahmadrezajafary1384@gmail.com>
2025-05-21 14:52:36 +0330

    Step 4 completed

--- Database-Workshop/src/main/java/org/project/DatabaseManager.java
+++ Database-Workshop/src/main/java/org/project/DatabaseManager.java
@@ -1,7 +1,7 @@
 package org.project;
 
 import org.project.Models.Player;
-
+import org.project.Models.Club;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -9,28 +9,95 @@ import java.sql.SQLException;
 
 public class DatabaseManager {
     // todo: modify these constants to connect to database
-    private static final String JDBC_URL = "jdbc:postgresql://localhost:5432/workshop";
+    private static final String JDBC_URL = "jdbc:postgresql://localhost:5432/postgres";
     private static final String USERNAME = "postgres";
     private static final String PASSWORD = "35546";
 
+
+
+
+
     // a method to connect to database with credentials given
-    private static Connection connect() throws SQLException {
+    private static Connection connect() throws SQLException, ClassNotFoundException {
+        Class.forName("org.postgresql.Driver"); // Loads the PostgreSQL JDBC driver
         return DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
     }
+    public static void createCLUB(Club club) throws SQLException, ClassNotFoundException {
 
-    public static void createPlayer(Player player) throws SQLException {
-        Connection conn = connect();
+        try {
+            Connection conn = connect();
+            String query = "INSERT INTO club(id , title , manager) VALUES (? , ? , ?)";
+
+
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setObject(1, club.getId());
+            ps.setString(2, club.getTitle());
+            ps.setString(3, club.getManager());
+
+            ps.executeUpdate();
+            ps.close();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+        public static void createPlayer(Player player) throws SQLException, ClassNotFoundException {
+
+
         // todo: fill the query to create a player
-        String query = "INSERT INTO player(id , full_name , club_id) VALUES (? , ? , ?)";
-        PreparedStatement ps = conn.prepareStatement(query);
-        ps.setObject(1 , player.getId());
-        ps.setString(2 , player.getFullName());
-        ps.setObject(3 , player.getClubId());
-        // todo: set values of the prepared statement
-        ps.executeUpdate();
-        ps.close();
-        conn.close();
+        try {
+            Connection conn = connect();
+            String query = "INSERT INTO player(id , full_name , club_id) VALUES (? , ? , ?)";
+
+
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setObject(1 , player.getId());
+            ps.setString(2 , player.getFullName());
+            ps.setObject(3 , player.getClubId());
+
+            ps.executeUpdate();
+            ps.close();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
+
+
+    public static void clubViewer() throws SQLException, ClassNotFoundException {
+        Connection conn = connect();
+        var st = conn.createStatement();
+        var rs = st.executeQuery("SELECT * FROM club");
+        System.out.println("--- Clubs ---");
+        while (rs.next()) {
+            System.out.printf("%s | %s | %s\n", rs.getObject("id"),
+                                                rs.getString("title"),
+                                                rs.getString("manager"));
+        }
+        System.out.println();
+        rs.close();
+        st.close();
+
+    }
+    public static void playerViewer() throws SQLException, ClassNotFoundException {
+        Connection conn = connect();
+        var st = conn.createStatement();
+        var rs = st.executeQuery("SELECT * FROM player");
+        System.out.println("--- Players ---");
+        while (rs.next()) {
+            System.out.printf("%s | %s | %s\n", rs.getObject("id"),
+                                                rs.getString("full_name"),
+                                                rs.getObject("club_id"));
+        }
+        System.out.println();
+        rs.close();
+        st.close();
+
+    }
+
+
+
     // todo: create methods to complete the task of workshop
+
 }
--- Database-Workshop/src/main/java/org/project/Main.java
+++ Database-Workshop/src/main/java/org/project/Main.java
@@ -2,20 +2,40 @@ package org.project;
 
 import org.project.Models.Club;
 import org.project.Models.Player;
+import org.project.DatabaseManager;
 
+import java.sql.SQLException;
 import java.util.UUID;
 
 public class Main {
-    public static void main(String[] args) {
-        // club instances
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
         Club club1 = new Club(UUID.randomUUID(), "Real Madrid", "Carlo Ancelotti");
+        DatabaseManager.createCLUB(club1);
+
         Club club2 = new Club(UUID.randomUUID(), "Liverpool", "Arne Slot");
+        DatabaseManager.createCLUB(club2);
+
         // player instances
         Player player1 = new Player(UUID.randomUUID(), "Thibaut Courtois", club1.getId());
-        Player player2 = new Player(UUID.randomUUID(), "Jude Bellingham", club1.getId());
-        Player player3 = new Player(UUID.randomUUID(), "Trent Alexander-Arnold", club2.getId());
-        Player player4 = new Player(UUID.randomUUID(), "Federico Valverde", club1.getId());
+        DatabaseManager.createPlayer(player1);
 
+        Player player2 = new Player(UUID.randomUUID(), "Jude Bellingham", club1.getId());
+        DatabaseManager.createPlayer(player2);
+
+        Player player3 = new Player(UUID.randomUUID(), "Trent Alexander-Arnold", club2.getId() );
+        DatabaseManager.createPlayer(player3);
+
+        Player player4 = new Player(UUID.randomUUID(), "Federico Valverde", club2.getId() );
+        DatabaseManager.createPlayer(player4);
         // todo: call methods to run queries
+
+        //🔍 Step 4: Select and View Data
+        DatabaseManager.clubViewer();
+
+        DatabaseManager.playerViewer();
+
+
+
     }
 }
